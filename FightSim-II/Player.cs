24656ea9f957e64834722b83e677e773810208c9
EDVIN TSYGANKOV <70744431+PoseironDire@users.noreply.github.com>
2021-09-20 09:33:56 +0200

    Initiate Repository & Player Class

---
+++ FightSim-II/Player.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FightSim_II
+{
+    public class Player
+    {
+        public string name;
+
+        public int hp;
+    }
+}
---
+++ FightSim-II/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FightSim_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p1 = new Player();
+            Player p2 = new Player();
+
+            //Players choose their names
+            Console.WriteLine("Welcome to Drip Arena PVP");
+            Console.WriteLine("Choose name for Player 1:");
+            p1.name = Console.ReadLine();
+            Console.WriteLine("Player 1 is called: " + p1.name + "!");
+            Console.WriteLine("Choose name for Player 2");
+            p1.name = Console.ReadLine();
+            Console.WriteLine("Player 2 is called: " + p2.name + "!");
+
+            Console.ReadLine();
+        }
+    }
+}
